da5752ff35b4ec7ce4ea3ff94627e39e3d1d3819
Kabeer Ahamed <kabeerahamedkm@gmail.com>
2024-07-09 22:00:53 +0530

    Added al files

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -4,36 +4,37 @@ import React from 'react';
 function Footer(props) {
     
     return (
-<footer className="text-center text-lg-start" style={{ backgroundColor: "#e4e4e4" }}>
-  <div className="container d-flex justify-content-center py-4">
-    <button
-      type="button"
-      className="btn btn-primary btn-lg btn-floating mx-2"
-      style={{ backgroundColor: "#3778c2", border: "none" }}
-    >
-      <i className="fab fa-facebook-f"></i>
-    </button>
-    <button
-      type="button"
-      className="btn btn-primary btn-lg btn-floating mx-2"
-      style={{ backgroundColor: "#3778c2", border: "none" }}
-    >
-      <i className="fab fa-youtube"></i>
-    </button>
-    <button
-      type="button"
-      className="btn btn-primary btn-lg btn-floating mx-2"
-      style={{ backgroundColor: "#3778c2", border: "none" }}
-    >
-      <i className="fab fa-instagram"></i>
-    </button>
-    <button
-      type="button"
-      className="btn btn-primary btn-lg btn-floating mx-2"
-      style={{ backgroundColor: "#3778c2", border: "none" }}
-    >
-      <i className="fab fa-twitter"></i>
-    </button>
+<footer className="text-center text-lg-start" style={{ backgroundColor: "#3778c2" }}>
+  <div className="container d-flex justify-content-center py-3">
+  <button
+  type="button"
+  className="btn btn-primary btn-sm btn-floating mx-2"
+  style={{ backgroundColor: "white", border: "none", color: "#3778c2", padding: "5px 10px", fontSize: "12px" }}
+>
+  <i className="fab fa-facebook-f"></i>
+</button>
+<button
+  type="button"
+  className="btn btn-primary btn-sm btn-floating mx-2"
+  style={{ backgroundColor: "white", border: "none", color: "#3778c2", padding: "5px 10px", fontSize: "12px" }}
+>
+  <i className="fab fa-youtube"></i>
+</button>
+<button
+  type="button"
+  className="btn btn-primary btn-sm btn-floating mx-2"
+  style={{ backgroundColor: "white", border: "none", color: "#3778c2", padding: "5px 10px", fontSize: "12px" }}
+>
+  <i className="fab fa-instagram"></i>
+</button>
+<button
+  type="button"
+  className="btn btn-primary btn-sm btn-floating mx-2"
+  style={{ backgroundColor: "white", border: "none", color: "#3778c2", padding: "5px 10px", fontSize: "12px" }}
+>
+  <i className="fab fa-twitter"></i>
+</button>
+
   </div>
   <div
     className="text-center text-black p-1.5"
